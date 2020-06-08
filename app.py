7e2314752c4c9871f4af48c0b772ba148a018558
Pedro Danna <pedrodanna.dev@gmail.com>
2020-06-08 04:36:57 -0300

    SQL database added

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 #Referencing this file
 app = Flask(__name__)
+
+#Database location
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#Initializing database
+db = SQLAlchemy(app)
+
+#DB Model class
+class Todo(db.Model):
+    #Primary Key
+    id = db.Column(db.Integer, primary_key=True)
+    #Content
+    content = db.Column(db.String(200), nullable = False)
+    #Date Created
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    #Function to return a string when new element is entered
+    def __repr__(self):
+        return "<Task %r>" % self.id
 
 #App URL
 @app.route('/')
