68b8c833ceb7ff40f83a886c33da0c980d7bc87e
Pedro Danna <pedrodanna.dev@gmail.com>
2020-06-08 15:56:38 -0300

    HTML and Python done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,15 +19,80 @@ class Todo(db.Model):
     #Date Created
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
-    #Function to return a string when new element is entered
+    #Function to return a string when new element is added
     def __repr__(self):
         return "<Task %r>" % self.id
 
 #App URL
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 #Website fucntion
 def index():
-    return render_template('index.html')
+    #Post request action
+    if request.method == 'POST':
+        #Variable to store form data
+        task_content = request.form['content']
+        #Creating database object
+        new_task = Todo(content=task_content)
+
+        #Push to database
+        try:
+            #Add item to db
+            db.session.add(new_task)
+            #Commit change
+            db.session.commit()
+            #Return to application
+            return redirect('/')
+        except:
+            #Report error
+            return 'Unable to add item to DB'
+
+    else:
+        #Variable with database data
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        #Run page with giving varibles
+        return render_template('index.html', tasks=tasks)
+
+#App URL to delete item
+@app.route('/delete/<int:id>')
+#Function to delete item
+def delete(id):
+    #Variable to store the id of the item to be deleted
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        #Delete item in db
+        db.session.delete(task_to_delete)
+        #Commit change
+        db.session.commit()
+        #Return to application
+        return redirect('/')
+
+    except:
+        #Report error
+        return 'Unable to delete task'
+
+#App URL to update item
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+#Function to update item
+def update(id):
+    #Variable to store task 
+    task = Todo.query.get_or_404(id)
+    #Post request action
+    if request.method == 'POST':
+        #Variable to store form data
+        task.content = request.form['content']
+
+        try:
+            #Commit changes
+            db.session.commit()
+            #Return to application
+            return redirect('/')
+        except:
+            return 'Unable to update task'
+
+    else:
+        #Run page with giving varibles
+        return render_template('update.html', task=task)
 
 #Executing the app
 if __name__ == "__main__":
